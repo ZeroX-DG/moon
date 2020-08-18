f55dbc2b796609826469f70823e10974043de21e
ZeroX-DG <viethungax@gmail.com>
2020-08-19 08:25:12 +1200

    initial implmentation of dom

--- components/dom/src/node.rs
+++ components/dom/src/node.rs
@@ -1,23 +1,105 @@
-use std::rc::Weak;
+use std::rc::{Weak, Rc};
+use std::cell::RefCell;
+use std::ops::{Deref};
 
-pub struct Node {
-    parent: Weak<Node>,
-    child_nodes: Vec<Node>
+#[derive(Debug, Clone)]
+pub enum NodeType {
+    Element = 1,
+    Text = 3,
+    CDataSection = 4,
+    ProcessingInstruction = 7,
+    Comment = 8,
+    Document = 9,
+    DocumentType = 10,
+    DocumentFragment = 11
 }
 
-impl Node {
-    pub fn new() -> Self {
-        Self {
-            parent: Weak::new(),
-            child_nodes: Vec::new()
+pub struct Node {
+    node_type: NodeType,
+    parent_node: Option<WeakNodeRef>,
+    first_child: Option<NodeRef>,
+    last_child: Option<WeakNodeRef>,
+    next_sibling: Option<NodeRef>,
+    prev_sibling: Option<WeakNodeRef>,
+}
+
+pub struct NodeRef(Rc<RefCell<Node>>);
+pub struct WeakNodeRef(Weak<RefCell<Node>>);
+
+impl Deref for NodeRef {
+    type Target = RefCell<Node>;
+
+    fn deref(&self) -> &RefCell<Node> {
+        &*self.0
+    }
+}
+
+impl Clone for WeakNodeRef {
+    fn clone(&self) -> Self {
+        Self(self.0.clone())        
+    }
+}
+
+impl Clone for NodeRef {
+    fn clone(&self) -> Self {
+        Self(self.0.clone())        
+    }
+}
+
+impl NodeRef {
+    pub fn downgrade(self) -> WeakNodeRef {
+        WeakNodeRef(Rc::downgrade(&self.0))
+    }
+
+    pub fn parent(&self) -> Option<NodeRef> {
+        let ref_self = self.borrow();
+        match &ref_self.parent_node {
+            Some(node) => node.clone().upgrade(),
+            _ => None
         }
     }
 
-    pub fn first_child(&self) -> Option<&Node> {
-        self.child_nodes.first()
+    pub fn next_sibling(&self) -> Option<NodeRef> {
+        let ref_self = self.borrow();
+        ref_self.next_sibling.clone()
     }
 
-    pub fn last_child(&self) -> Option<&Node> {
-        self.child_nodes.last()
+    pub fn prev_sibling(&self) -> Option<NodeRef> {
+        let ref_self = self.borrow();
+        match &ref_self.prev_sibling {
+            Some(node) => node.clone().upgrade(),
+            _ => None
+        }
     }
+
+    pub fn first_child(&self) -> Option<NodeRef> {
+        let ref_self = self.borrow();
+        ref_self.first_child.clone()
+    }
+
+    pub fn last_child(&self) -> Option<NodeRef> {
+        let ref_self = self.borrow();
+        match &ref_self.last_child {
+            Some(node) => node.clone().upgrade(),
+            _ => None
+        }
+    }
+
+    pub fn node_type(&self) -> NodeType {
+        let ref_self = self.borrow();
+        ref_self.node_type.clone()
+    }
+}
+
+impl WeakNodeRef {
+    pub fn upgrade(self) -> Option<NodeRef> {
+        match self.0.upgrade() {
+            Some(node) => Some(NodeRef(node)),
+            _ => None
+        }
+    }
+}
+
+#[cfg(test)]
+mod test {
 }
