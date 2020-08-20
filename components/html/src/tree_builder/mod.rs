0dbb22216fcc95cce6c3e520881f81dde9c14a34
ZeroX-DG <viethungax@gmail.com>
2020-08-21 01:50:23 +1200

    added more insert mode

--- components/html/src/tree_builder/mod.rs
+++ components/html/src/tree_builder/mod.rs
@@ -1,13 +1,18 @@
+mod insert_mode;
+
 use dom::dom_traits::Dom;
+use insert_mode::InsertMode;
 
 pub struct TreeBuilder<T: Dom> {
-    open_elements: Vec<T>
+    open_elements: Vec<T>,
+    insert_mode: InsertMode
 }
 
 impl<T: Dom> TreeBuilder<T> {
     pub fn new() -> Self {
         Self {
-            open_elements: Vec::new()
+            open_elements: Vec::new(),
+            insert_mode: InsertMode::Initial
         }
     }
 }
