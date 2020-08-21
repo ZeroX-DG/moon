474a13777791483876470fde2d60ca9e8578f2e1
ZeroX-DG <viethungax@gmail.com>
2020-08-21 16:32:17 +1200

    added tree building status

--- components/html/src/tree_builder/mod.rs
+++ components/html/src/tree_builder/mod.rs
@@ -9,11 +9,27 @@ pub struct TreeBuilder {
     insert_mode: InsertMode
 }
 
+pub enum TreeBuildingStatus {
+    Continue,
+    Stop
+}
+
 impl TreeBuilder {
     pub fn new() -> Self {
         Self {
             open_elements: Vec::new(),
             insert_mode: InsertMode::Initial
         }
+    }
+
+    pub fn feed(&mut self, token: Token) -> TreeBuildingStatus {
+        match self.insert_mode {
+            InsertMode::Initial => self.handle_initial(token),
+            _ => unimplemented!()
+        }
+    }
+
+    fn handle_initial(&mut self, token: Token) -> TreeBuildingStatus {
+        TreeBuildingStatus::Continue
     }
 }
