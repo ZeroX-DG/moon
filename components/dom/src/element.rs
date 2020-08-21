2db33ac20a681096cc7b2006c64f1c194073f79c
ZeroX-DG <viethungax@gmail.com>
2020-08-21 13:48:49 +1200

    updated element node

--- components/dom/src/element.rs
+++ components/dom/src/element.rs
@@ -1,30 +1,39 @@
-use std::cell::RefCell;
-use std::rc::Rc;
 use std::ops::Deref;
 use super::dom_token_list::DOMTokenList;
 use super::node::NodeData;
 
 pub struct Element<T: ElementData> {
-    inner: Rc<RefCell<T>>,
+    inner: T,
     tag_name: String,
     id: String,
     class_list: DOMTokenList
 }
 
 impl<T: ElementData> Element<T> {
+    pub fn tag_name(&self) -> String {
+        self.tag_name.clone()
+    }
+
+    pub fn id(&self) -> String {
+        self.id.clone()
+    }
+
+    pub fn class_list(&self) -> &DOMTokenList {
+        &self.class_list
+    }
+
     pub fn class_name(&self) -> String {
         self.class_list.value()
     }
 }
 
 impl<T: ElementData> Deref for Element<T> {
-    type Target = Rc<RefCell<T>>;
+    type Target = T;
 
     fn deref(&self) -> &Self::Target {
         &self.inner
     }
 }
-
 
 pub trait ElementData {}
 
