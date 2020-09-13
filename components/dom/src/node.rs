ed7e51e8cdec5ba3bf27fa57d2bb8f96358b6f84
ZeroX-DG <viethungax@gmail.com>
2020-09-13 15:21:20 +1200

    added docs for node type

--- components/dom/src/node.rs
+++ components/dom/src/node.rs
@@ -22,14 +22,17 @@ impl Node {
         }
     }
 
+    /// Set the owner document for node
     pub fn set_document(&mut self, doc: WeakNodeRef) {
         self.owner_document = Some(doc);
     }
 
+    /// First child of the node
     pub fn first_child(&self) -> Option<NodeRef> {
         self.first_child.clone()
     }
 
+    /// Last child of the node
     pub fn last_child(&self) -> Option<NodeRef> {
         match &self.last_child {
             Some(node) => node.clone().upgrade(),
@@ -37,10 +40,12 @@ impl Node {
         }
     }
 
+    /// Next sibling of the node
     pub fn next_sibling(&self) -> Option<NodeRef> {
         self.next_sibling.clone()
     }
 
+    /// Previous sibling of the node
     pub fn prev_sibling(&self) -> Option<NodeRef> {
         match &self.prev_sibling {
             Some(node) => node.clone().upgrade(),
@@ -48,6 +53,7 @@ impl Node {
         }
     }
 
+    /// Owner document of the node
     pub fn owner_document(&self) -> Option<NodeRef> {
         match &self.owner_document {
             Some(node) => node.clone().upgrade(),
@@ -55,6 +61,14 @@ impl Node {
         }
     }
 
+    /// Append a child node to a parent node
+    ///
+    /// **Ensure that:**
+    /// 1. Last child of the parent is the child
+    /// 2. First child of the parent is also the child if the parent has only 1 child
+    /// 3. The child parent is this parent
+    /// 4. The next-to-last child of the parent next sibling is the child if the parent has more
+    ///    than 1 child
     pub fn append_child(parent: NodeRef, child: NodeRef) {
         if let Some(last_child) = parent.borrow().as_node().last_child() {
             last_child.borrow_mut().as_node_mut().next_sibling = Some(child.clone());
@@ -69,6 +83,17 @@ impl Node {
         parent.borrow_mut().as_node_mut().last_child = Some(child.downgrade());
     }
 
+    /// Insert a child node to a parent node before a reference child node
+    ///
+    /// **Ensure that:**
+    /// 1. The previous sibling of reference child is the child node if reference child is present
+    /// 2. If the reference child is not present, append the child to the parent
+    /// 3. The first child of the parent is the child if the parent contains only the reference
+    ///    child
+    /// 4. The next sibling of the child before the reference child is the inserted child
+    /// 5. The next sibling of the inserted child is the reference child
+    /// 6. The previous sibling of the inserted node is the node before the reference node
+    /// 7. The parent of the inserted child is the parent
     pub fn insert_before(parent: NodeRef, child: NodeRef, ref_child: Option<NodeRef>) {
         if let Some(ref_child) = ref_child {
             let mut child_ref = child.borrow_mut();
