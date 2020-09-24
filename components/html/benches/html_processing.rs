b5d5e25c4cd0a6571e29e83ab85b70f2a2762842
ZeroX-DG <viethungax@gmail.com>
2020-09-25 02:37:03 +1200

    added benchmark for html

---
+++ components/html/benches/html_processing.rs
@@ -0,0 +1,15 @@
+use criterion::{black_box, criterion_group, criterion_main, Criterion};
+use html::tokenizer::Tokenizer;
+use html::tree_builder::TreeBuilder;
+
+fn html_parsing_benchmark(c: &mut Criterion) {
+    let html = include_str!("./purecss_gaze.html");
+    c.bench_function("parse_purecss_gaze", |b| b.iter(|| {
+        let tokenizer = Tokenizer::new(black_box(html.to_string()));
+        let tree_builder = TreeBuilder::new(tokenizer);
+        tree_builder.run();
+    }));
+}
+
+criterion_group!(benches, html_parsing_benchmark);
+criterion_main!(benches);
