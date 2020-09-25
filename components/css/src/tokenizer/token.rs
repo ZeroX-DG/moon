3b8777aad485f0938e8edcbc4549a6442ba15be6
ZeroX-DG <viethungax@gmail.com>
2020-09-25 12:39:26 +1200

    added css tokenizer folder

---
+++ components/css/src/tokenizer/token.rs
@@ -0,0 +1,44 @@
+pub enum Token {
+    Ident(String),
+    Function(String),
+    AtKeyword(String),
+    Hash(String, HashType),
+    Str(String),
+    BadStr,
+    Url(String),
+    BadUrl,
+    Delim(char),
+    Number {
+        value: usize,
+        type_: NumberType
+    },
+    Percentage(usize),
+    Dimension {
+        value: usize,
+        type_: NumberType,
+        unit: String
+    },
+    Whitespace,
+    CDO,
+    CDC,
+    Colon,
+    Semicolon,
+    Comma,
+    BracketOpen,
+    BracketClose,
+    ParentheseOpen,
+    ParentheseClose,
+    BraceOpen,
+    BraceClose,
+    EOF
+}
+
+pub enum HashType {
+    Id,
+    Unrestricted
+}
+
+pub enum NumberType {
+    Integer,
+    Number
+}
