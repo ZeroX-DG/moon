6cd719a74abbd983b2e8142c0b5a62d2ce6a0d22
ZeroX-DG <viethungax@gmail.com>
2020-12-19 13:00:17 +1300

    refactored display value

--- components/style/src/values/display.rs
+++ components/style/src/values/display.rs
@@ -3,29 +3,64 @@ use css::tokenizer::token::Token;
 
 #[derive(Debug, Clone, Eq, PartialEq, Hash)]
 pub enum Display {
-    Inline,
+    Full(OuterDisplayType, InnerDisplayType),
+    Box(DisplayBox)
+}
+
+#[derive(Debug, Clone, Eq, PartialEq, Hash)]
+pub enum OuterDisplayType {
     Block,
-    InlineBlock,
-    ListItem,
+    Inline,
+    RunIn
+}
+
+#[derive(Debug, Clone, Eq, PartialEq, Hash)]
+pub enum InnerDisplayType {
+    Flow,
+    FlowRoot,
     Table,
-    InlineTable,
-    None,
+    Flex,
+    Grid
+}
+
+#[derive(Debug, Clone, Eq, PartialEq, Hash)]
+pub enum DisplayBox {
+    Contents,
+    None
+}
+
+macro_rules! match_ident {
+    ($value:expr, {$($matcher:expr => $result:expr),*}) => {
+        match $value {
+            ComponentValue::PerservedToken(Token::Ident(v)) => match v {
+                $(
+                    v if v.eq_ignore_ascii_case($matcher) => Some($result)
+                ),*,
+                _ => None
+            }
+            _ => None
+        }
+    };
 }
 
 impl Display {
     pub fn parse(values: &[ComponentValue]) -> Option<Self> {
-        match values.iter().next() {
-            Some(ComponentValue::PerservedToken(Token::Ident(value))) => match value {
-                v if v.eq_ignore_ascii_case("inline") => Some(Display::Inline),
-                v if v.eq_ignore_ascii_case("block") => Some(Display::Block),
-                v if v.eq_ignore_ascii_case("inline-block") => Some(Display::InlineBlock),
-                v if v.eq_ignore_ascii_case("list-item") => Some(Display::ListItem),
-                v if v.eq_ignore_ascii_case("table") => Some(Display::Table),
-                v if v.eq_ignore_ascii_case("inline-table") => Some(Display::InlineTable),
-                v if v.eq_ignore_ascii_case("none") => Some(Display::None),
-                _ => None,
-            },
-            _ => None,
+        match values.len() {
+            1 => match_ident!(&values[0], {
+                "none" => Display::Box(DisplayBox::None),
+                "contents" => Display::Box(DisplayBox::Contents),
+                "block" => Self::new_block(),
+                "inline" => Self::new_inline()
+            }),
+            _ => None
         }
+    }
+
+    pub fn new_block() -> Self {
+        Display::Full(OuterDisplayType::Block, InnerDisplayType::Flow)
+    }
+
+    pub fn new_inline() -> Self {
+        Display::Full(OuterDisplayType::Inline, InnerDisplayType::Flow)
     }
 }
