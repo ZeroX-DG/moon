f0f24c1cb0320061bda16cdc87af48cb8b4a3d4d
ZeroX-DG <viethungax@gmail.com>
2020-12-26 16:26:53 +1300

    wip tree builder

--- components/layout/src/layout_box.rs
+++ components/layout/src/layout_box.rs
@@ -14,7 +14,7 @@ pub struct LayoutBox {
     pub dimensions: Dimensions,
 
     /// The render node that generate this box
-    pub render_node: RenderNodeRef,
+    pub render_node: Option<RenderNodeRef>,
 
     /// The formatting context that this block establish
     pub formatting_context: Option<FormattingContext>,
@@ -37,24 +37,36 @@ pub enum BoxType {
     Block,
 
     /// Inline-level box
-    Inline,
-
-    /// Anonymous inline-level box
-    AnonymousInline,
-
-    /// Anonymous block-level box
-    AnonymousBlock,
+    Inline
 }
 
 impl LayoutBox {
     pub fn new(node: RenderNodeRef, box_type: BoxType) -> Self {
         Self {
             box_type,
-            render_node: node,
+            render_node: Some(node),
             dimensions: Dimensions::default(),
             formatting_context: None,
             children: Vec::new(),
         }
+    }
+
+    pub fn new_anonymous(box_type: BoxType) -> Self {
+        Self {
+            box_type,
+            render_node: None,
+            dimensions: Dimensions::default(),
+            formatting_context: None,
+            children: Vec::new()
+        }
+    }
+
+    pub fn is_anonymous(&self) -> bool {
+        self.render_node.is_none()
+    }
+
+    pub fn is_inline(&self) -> bool {
+        self.box_type == BoxType::Inline
     }
 
     pub fn box_model(&mut self) -> &mut Dimensions {
@@ -77,16 +89,31 @@ impl LayoutBox {
 fn dump_layout_tree(root: &LayoutBox, level: usize) -> String {
     let mut result = String::new();
     let child_nodes = &root.children;
-    result.push_str(&format!(
-        "{}{:#?}({:#?})(x: {} | y: {} | width: {} | height: {})\n",
-        "    ".repeat(level),
-        root.box_type,
-        root.render_node.borrow().node,
-        root.dimensions.content.x,
-        root.dimensions.content.y,
-        root.dimensions.content.width,
-        root.dimensions.content.height
-    ));
+
+    let formatting_context = match &root.formatting_context {
+        Some(s) => format!("{:?} Formatting Context", s),
+        None => format!("no formatting context")
+    };
+
+    if let Some(node) = &root.render_node {
+        result.push_str(&format!(
+            "{}[{:?}] {:#?} establish {}\n",
+            "  ".repeat(level),
+            root.box_type,
+            node.borrow().node,
+            formatting_context
+        ));
+    }
+    else {
+        result.push_str(&format!(
+            "{}[Anonymous {:?}] establish {}\n",
+            "  ".repeat(level),
+            root.box_type,
+            formatting_context
+        ));
+    }
+
+    
     for node in child_nodes {
         result.push_str(&dump_layout_tree(node, level + 1));
     }
--- components/layout/src/tree_builder.rs
+++ components/layout/src/tree_builder.rs
@@ -6,20 +6,23 @@ use super::layout_box::{LayoutBox, BoxType, FormattingContext};
 use style::render_tree::RenderNodeRef;
 use style::value_processing::{Property, Value};
 use style::values::display::{Display, OuterDisplayType, InnerDisplayType};
+use std::rc::Rc;
+use std::cell::RefCell;
 
 pub struct TreeBuilder {
-    parent_stack: Vec<*mut LayoutBox>,
+    parent_stack: Rc<RefCell<Vec<*mut LayoutBox>>>,
     root: RenderNodeRef
 }
 
 impl TreeBuilder {
     pub fn new(root: RenderNodeRef) -> Self {
         Self {
-            parent_stack: Vec::new(),
+            parent_stack: Rc::new(RefCell::new(Vec::new())),
             root
         }
     }
 
+    /// Build the layout tree for the provided root render node
     pub fn build(mut self) -> Option<LayoutBox> {
         let root = self.root.clone();
         let mut root_box = match build_box_by_display(&root) {
@@ -27,38 +30,132 @@ impl TreeBuilder {
             None => return None
         };
 
+        self.parent_stack.borrow_mut().push(&mut root_box);
         for child in &root.borrow().children {
-            self.parent_stack.push(&mut root_box);
             self.build_layout_tree(child.clone());
-            self.parent_stack.pop();
         }
+        self.parent_stack.borrow_mut().pop();
 
         return Some(root_box);
     }
 
-    pub fn build_layout_tree(&mut self, node: RenderNodeRef) -> Option<&LayoutBox> {
-        let mut layout_box = match build_box_by_display(&node) {
+    /// Recursively building the layout tree for a node
+    fn build_layout_tree(&mut self, node: RenderNodeRef) -> Option<&LayoutBox> {
+        let layout_box = match build_box_by_display(&node) {
             Some(b) => b,
             None => return None
         };
 
+        let parent =  unsafe {
+            if layout_box.is_inline() {
+                get_parent_for_inline(self.parent_stack.clone())
+            } else {
+                get_parent_for_block(self.parent_stack.clone())
+            }
+        };
+
+        parent.add_child(layout_box);
+
+        let box_ref = parent.children.last_mut().unwrap();
+
+        self.parent_stack.borrow_mut().push(box_ref);
         for child in &node.borrow().children {
-            self.parent_stack.push(&mut layout_box);
             self.build_layout_tree(child.clone());
-            self.parent_stack.pop();
         }
+        self.parent_stack.borrow_mut().pop();
 
-        unsafe {
-            let parent = self.parent_stack
-                .last()
-                .expect("No parent in stack")
-                .as_mut()
-                .expect("Can't get mutable reference to parent");
-            parent.add_child(layout_box);
-
-            parent.children.last()
-        }
+        parent.children.last()
     }
+}
+
+/// Get a parent for an inline-level box
+///
+/// An inline-level box can be inserted into the nearest parent.
+///
+/// If the nearest parent established an inline formatting context, then
+/// insert the box as a direct children of the parent.
+///
+/// Otherwise, if the nearest parent established a block formatting context
+/// then create an anonymous block-level box to wrap the inline-box in before
+/// inserting into the parent.
+unsafe fn get_parent_for_inline<'a>(parent_stack: Rc<RefCell<Vec<*mut LayoutBox>>>) -> &'a mut LayoutBox {
+    let parent_stack = parent_stack.borrow();
+    let parent = parent_stack
+        .last()
+        .expect("No parent in stack");
+
+    let formatting_context = &parent
+        .as_ref()
+        .unwrap()
+        .formatting_context;
+
+    let parent_mut = parent
+        .as_mut()
+        .expect("Can't get mutable reference to parent");
+
+    if let Some(FormattingContext::Inline) = formatting_context {
+        return parent_mut;
+    }
+
+    if let Some(last) = parent_mut.children.last() {
+        if !last.is_anonymous() || last.formatting_context != Some(FormattingContext::Inline) {
+            let mut anonymous = LayoutBox::new_anonymous(BoxType::Block);
+            anonymous.set_formatting_context(FormattingContext::Inline);
+            parent_mut.add_child(anonymous);
+        }
+    } else {
+        let mut anonymous = LayoutBox::new_anonymous(BoxType::Block);
+        anonymous.set_formatting_context(FormattingContext::Inline);
+        parent_mut.add_child(anonymous);
+    }
+
+    parent_mut.children.last_mut().unwrap()
+}
+
+/// Get a parent for an block-level box
+///
+/// A block-level box can only be inserted into the nearest non-inline parent.
+///
+/// If the parent established a non-inline formatting context, then
+/// insert the box as a direct children of the parent.
+///
+/// Otherwise, if the nearest parent established an inline formatting
+/// context, then create an anonymous block-level box to wrap all the
+/// inline-level boxes currently in the parent. After that, set the
+/// formatting context of parent to block and insert the box as a direct
+/// children of the parent.
+unsafe fn get_parent_for_block<'a>(parent_stack: Rc<RefCell<Vec<*mut LayoutBox>>>) -> &'a mut LayoutBox {
+    let parent_stack = parent_stack.borrow();
+    let mut index = parent_stack.len() - 1;
+    let mut parent = parent_stack[index];
+
+    while let BoxType::Inline = parent.as_ref().unwrap().box_type {
+        if index == 0 {
+            panic!("No block parent found")
+        }
+        index -= 1;
+        parent = parent_stack[index];
+    }
+
+    let formatting_context = &parent
+        .as_ref()
+        .unwrap()
+        .formatting_context;
+
+    let parent_mut = parent
+        .as_mut()
+        .expect("Can't get mutable reference to parent");
+
+    if let Some(FormattingContext::Inline) = formatting_context {
+        let children = parent_mut.children.drain(..).collect::<Vec<_>>();
+        let mut anonymous = LayoutBox::new_anonymous(BoxType::Block);
+        anonymous.children = children;
+        anonymous.set_formatting_context(FormattingContext::Inline);
+        parent_mut.add_child(anonymous);
+        parent_mut.set_formatting_context(FormattingContext::Block);
+    }
+
+    return parent_mut;
 }
 
 fn establish_inline_context(node: &RenderNodeRef) -> bool {
@@ -117,12 +214,17 @@ mod tests {
 
     #[test]
     fn test_build_simple() {
-        let dom = element("div#parent", vec![
-            element("span", vec![
+        let dom = element("div", vec![
+            element("span", vec![]),
+            element("p", vec![
+                element("span", vec![]),
+                element("span", vec![]),
+                element("span", vec![])
             ])
         ]);
 
-        let css = r#"div {
+        let css = r#"
+        p, div {
             display: block;
         }
         span {
@@ -148,6 +250,46 @@ mod tests {
 
         let layout_box = layout_tree_builder.build();
 
-        println!("{:#?}", layout_box.unwrap().to_string());
+        println!("{}", layout_box.unwrap().to_string());
+    }
+
+    #[test]
+    fn test_block_break_inline() {
+        let dom = element("div", vec![
+            element("span", vec![
+                element("span", vec![]),
+                element("p", vec![]),
+                element("a", vec![])
+            ])
+        ]);
+
+        let css = r#"
+        p, div {
+            display: block;
+        }
+        span, a {
+            display: inline;
+        }"#;
+
+        let stylesheet = parse_stylesheet(css);
+
+        let rules = stylesheet
+            .iter()
+            .map(|rule| match rule {
+                CSSRule::Style(style) => ContextualRule {
+                    inner: style,
+                    location: CSSLocation::Embedded,
+                    origin: CascadeOrigin::User,
+                },
+            })
+            .collect::<Vec<ContextualRule>>();
+
+        let render_tree = build_render_tree(dom.clone(), &rules);
+
+        let layout_tree_builder = TreeBuilder::new(render_tree.root.unwrap());
+
+        let layout_box = layout_tree_builder.build();
+
+        println!("{}", layout_box.unwrap().to_string());
     }
 }
