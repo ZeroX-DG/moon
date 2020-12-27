43725f33aa3e72e9c8b20eed272f4c39b15a0b10
ZeroX-DG <viethungax@gmail.com>
2020-12-28 04:26:25 +1300

    updated flow layout to support right behavior

--- components/layout/src/flow/block.rs
+++ components/layout/src/flow/block.rs
@@ -6,7 +6,11 @@ use crate::layout_box::LayoutBox;
 /// to put the next box at. Since every box in BFC has full width & layout
 /// from top to bottom, we don't need to update width or offsett x.
 pub(crate) fn update_context(root: &LayoutBox, context: &mut LayoutContext) {
-    let child_margin_height = root.dimensions.margin_box_height();
-    context.height += child_margin_height;
-    context.offset_y += child_margin_height - root.dimensions.margin.bottom;
+    let rect = root.dimensions.margin_box();
+    context.height += rect.height;
+    context.offset_y += rect.height - root.dimensions.margin.bottom;
+
+    if context.width < rect.width {
+        context.width = rect.width;
+    }
 }
--- components/layout/src/flow/inline.rs
+++ components/layout/src/flow/inline.rs
@@ -3,7 +3,11 @@ use crate::layout_box::LayoutBox;
 
 /// Update layout context for each box in inline formatting context.
 pub(crate) fn update_context(root: &LayoutBox, context: &mut LayoutContext) {
-    let child_margin_width = root.dimensions.margin_box_width();
-    context.width += child_margin_width;
-    context.offset_x += child_margin_width - root.dimensions.margin.right;
+    let rect = root.dimensions.margin_box();
+    context.width += rect.width;
+    context.offset_x += rect.width - root.dimensions.margin.right;
+
+    if context.height < rect.height {
+        context.height = rect.height;
+    }
 }
