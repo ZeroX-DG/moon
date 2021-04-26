1954fccfe6936262e3acbf8d749498f5ff8843e1
Nguyen Viet Hung <viethungax@gmail.com>
2021-04-27 02:07:36 +1200

    Support for border rendering (#46)
    * added support for border rendering
    
    * formatted code

--- components/painting/src/lib.rs
+++ components/painting/src/lib.rs
@@ -6,7 +6,7 @@ mod primitive;
 mod render;
 mod utils;
 
-use command::DisplayCommand;
+use command::{DisplayCommand, DrawCommand};
 use layout::layout_box::LayoutBox;
 use render::PaintChainBuilder;
 
@@ -16,19 +16,31 @@ pub use primitive::*;
 pub use render::DisplayList;
 
 use paint_functions::background::paint_background;
+use paint_functions::border::paint_border;
 
 pub fn paint(display_list: &DisplayList, painter: &mut dyn Painter) {
     for command in display_list {
         match command {
-            DisplayCommand::FillRect(rect, color) => painter.fill_rect(rect, color),
-            DisplayCommand::FillRRect(rect, color) => painter.fill_rrect(rect, color),
-            _ => {}
+            DisplayCommand::Draw(draw_command) => draw(draw_command, painter),
+            DisplayCommand::GroupDraw(draw_commands) => {
+                for draw_command in draw_commands {
+                    draw(draw_command, painter);
+                }
+            }
         }
+    }
+}
+
+fn draw(draw_command: &DrawCommand, painter: &mut dyn Painter) {
+    match draw_command {
+        DrawCommand::FillRect(rect, color) => painter.fill_rect(rect, color),
+        DrawCommand::FillRRect(rect, color) => painter.fill_rrect(rect, color),
     }
 }
 
 pub fn build_display_list(layout_box: &LayoutBox) -> DisplayList {
     let chain = PaintChainBuilder::new_chain()
+        .with_function(&paint_border)
         .with_function(&paint_background)
         .build();
 
