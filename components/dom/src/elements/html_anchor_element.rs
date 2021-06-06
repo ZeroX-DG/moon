928d7570b7aeaffe9ddfb976dd17e653d77650b1
ZeroX-DG <viethungax@gmail.com>
2021-06-07 11:54:20 +1200

    added example anchor element

--- components/dom/src/elements/html_anchor_element.rs
+++ components/dom/src/elements/html_anchor_element.rs
@@ -1,18 +1,27 @@
 use super::ElementHooks;
 use super::ElementMethods;
 use crate::node::NodeHooks;
+use url::Url;
 
 #[derive(Debug)]
 pub struct HTMLAnchorElement {
+    href: Option<Url>
 }
 
 impl HTMLAnchorElement {
     pub fn empty() -> Self {
-        Self {}
+        Self {
+            href: None
+        }
     }
 }
 
 impl ElementHooks for HTMLAnchorElement {
+    fn on_attribute_change(&mut self, attr: &str, value: &str) {
+        if attr == "href" {
+            self.href = Url::parse(value).ok();
+        }
+    }
 }
 
 impl NodeHooks for HTMLAnchorElement {}
@@ -22,3 +31,4 @@ impl ElementMethods for HTMLAnchorElement {
         "a"
     }
 }
+
