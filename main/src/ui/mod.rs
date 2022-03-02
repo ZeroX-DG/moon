4d38e41843682961413b96e7a9189a937282a158
ZeroX-DG <viethungax@gmail.com>
2022-03-03 02:03:51 +1300

    Improved browser UI

--- main/src/ui/mod.rs
+++ main/src/ui/mod.rs
@@ -1,14 +1,15 @@
-use gtk::prelude::*;
-use gtk::{Application, ApplicationWindow, Entry, HeaderBar, Image};
-use url::parser::URLParser;
+mod primary_bar;
 
-use crate::app::get_app_runtime;
+use gtk::{prelude::*, Orientation};
+use gtk::{Application, ApplicationWindow, Image};
+
+use self::primary_bar::PrimaryBar;
 
 pub struct UI {
     pub app: Application,
     pub window: ApplicationWindow,
-    pub url_entry: Entry,
     pub content_area: Image,
+    pub primary_bar: PrimaryBar,
 }
 
 impl UI {
@@ -20,35 +21,21 @@ impl UI {
             .default_height(600)
             .build();
 
-        let url_entry = Entry::builder()
-            .placeholder_text("Enter URL")
-            .primary_icon_name("search-symbolic")
-            .hexpand(true)
-            .build();
-
         let content_area = Image::builder().hexpand(true).vexpand(true).build();
 
-        let header_bar = HeaderBar::builder().show_close_button(true).build();
+        let container = gtk::Box::builder()
+            .orientation(Orientation::Vertical)
+            .build();
+        let primary_bar = PrimaryBar::new(&container);
 
-        url_entry.connect_activate(|entry| {
-            let raw_url = entry.text().to_string();
-            get_app_runtime().update_state(move |state| {
-                let parse_url_result = URLParser::parse(&raw_url, None);
-                if let Some(url) = parse_url_result {
-                    state.active_tab_mut().goto(url);
-                }
-            });
-        });
-
-        header_bar.set_custom_title(Some(&url_entry));
-        window.set_titlebar(Some(&header_bar));
-        window.add(&content_area);
+        container.add(&content_area);
+        window.add(&container);
 
         Self {
             app,
             window,
-            url_entry,
             content_area,
+            primary_bar,
         }
     }
 }
