ba37ffcbdd78eeaf6fd7e791028532fd024da3dd
ZeroX-DG <viethungax@gmail.com>
2022-04-30 12:06:53 +1200

    compute absolute values

--- components/style/src/compute.rs
+++ components/style/src/compute.rs
@@ -1,15 +1,67 @@
 use std::collections::HashMap;
 
 use dom::node::NodePtr;
-use style_types::{ContextualRule, Property, Value};
+use style_types::{ContextualRule, Property, Value, values::{length::LengthUnit, prelude::{Color, Length, Percentage}}};
 
 use crate::cascade::collect_cascaded_values;
 
 pub fn compute_styles(node: NodePtr, rules: &[ContextualRule]) -> HashMap<Property, Value> {
     let mut styles = collect_cascaded_values(&node, rules);
 
+    compute_absolute_values(&node, &mut styles);
     compute_default_values(&node, &mut styles);
     styles
+}
+
+fn compute_absolute_values(node: &NodePtr, styles: &mut HashMap<Property, Value>) {
+    let base_font_size = 16.;
+    let parent_font_size = node
+        .parent()
+        .map(|parent| NodePtr(parent).get_style(&Property::FontSize).to_absolute_px())
+        .unwrap_or(base_font_size); 
+
+    let root_font_size = node
+        .owner_document()
+        .map(|root| NodePtr(root).get_style(&Property::FontSize).to_absolute_px())
+        .unwrap_or(base_font_size);
+
+    let mut updates = Vec::new();
+    for (property, value) in styles.iter() {
+        match value {
+            Value::Length(length) => match length {
+                Length { value, unit: LengthUnit::Em } => {
+                    let abs_length = Length::new_px(value.0 * parent_font_size);
+                    updates.push((property.clone(), Value::Length(abs_length)));
+                },
+                Length { value, unit: LengthUnit::Rem } => {
+                    let abs_length = Length::new_px(value.0 * root_font_size);
+                    updates.push((property.clone(), Value::Length(abs_length)));
+                },
+                _ => {}
+            }
+            Value::Percentage(percentage) => match percentage {
+                Percentage(value) if matches!(property, Property::FontSize) => {
+                    let abs_length = Length::new_px(value.0 * parent_font_size / 100.);
+                    updates.push((property.clone(), Value::Length(abs_length)));
+                }
+                _ => {}
+            }
+            Value::Color(color) => match color {
+                Color::CurrentColor => {
+                    let color = node.parent()
+                        .map(|p| p.get_style(&Property::Color))
+                        .unwrap_or(Value::initial(&Property::Color));
+                    updates.push((property.clone(), color));
+                }
+                _ => {}
+            }
+            _ => {}
+        }
+    }
+
+    for (property, value) in updates {
+        styles.insert(property, value);
+    }
 }
 
 fn compute_default_values(node: &NodePtr, styles: &mut HashMap<Property, Value>) {
