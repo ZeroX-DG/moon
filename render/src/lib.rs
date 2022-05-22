0e530d6a1b921244cd624499a2c8ad5c8a9ef22b
ZeroX-DG <viethungax@gmail.com>
2022-05-23 02:03:58 +1200

    removed redundant methods

--- render/src/lib.rs
+++ render/src/lib.rs
@@ -2,22 +2,5 @@ mod frame;
 mod page;
 mod renderer;
 
-use gfx::Bitmap;
-use shared::primitive::Size;
-use url::Url;
-
 pub use renderer::*;
 
-pub fn version() -> &'static str {
-    env!("CARGO_PKG_VERSION")
-}
-
-pub async fn render_once(html: String, base_url: Url, size: Size) -> Bitmap {
-    let mut renderer = Renderer::new().await;
-
-    renderer.initialize(RendererInitializeParams { viewport: size });
-
-    renderer.load_html(html, base_url);
-
-    renderer.output().await
-}
